598439c814b6284dcf3825da21dfef2d7f103efb
Adrián <adrian.blazquez.leon@alumnos.upm.es>
2020-06-26 21:37:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
         # Send the html message
-        output = 'New!!!!'
+        output = 'Adrian + app '
         self.wfile.write(output.encode('utf-8'))
 
 try:
